8437fb7c843486d0ef860f966e24117bbf452d7e
rafa913 <rafaellacerda60@gmail.com>
2026-06-28 17:51:24 -0400

    feat: adiciona componente Logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import './styles/global.css'
 import './styles/theme.css'
 
-import { Heading } from './components/Heading'
-import { Container } from './components/Container'
+import { Heading } from './components/Heading/Heading'
+import { Container } from './components/Container/Container'
+import { Logo } from './components/Logo/Logo'
 
 
 export default function App() {
@@ -11,7 +12,7 @@ export default function App() {
   return (
     <>
       <Container>
-        <Heading>Logo</Heading>
+        <Logo />
       </Container>
 
       <Container>
