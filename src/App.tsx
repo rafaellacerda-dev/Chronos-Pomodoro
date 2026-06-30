a9087bde6a62015d710c51408d5e1a31b38f1f5d
rafa913 <rafaellacerda60@gmail.com>
2026-06-30 19:02:50 -0400

    feat: Adiciona contador

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Heading } from './components/Heading/Heading'
 import { Container } from './components/Container/Container'
 import { Logo } from './components/Logo/Logo'
 import { Menu } from './components/Menu/Menu'
+import { CountDown } from './components/CountDown/CountDown'
 
 
 export default function App() {
@@ -21,6 +22,11 @@ export default function App() {
       </Container>
 
       <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+
         <Heading>#</Heading>
       </Container>
     </>
